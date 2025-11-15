6e5664779dd5fd966f53b133e8bdbac8c0502b19
Evandro Acorsi Filho <evandroaf10@hotmail.com>
2025-11-15 15:29:21 -0300

    Teste

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    
+
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
